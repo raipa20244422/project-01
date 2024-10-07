e422bda0f5a4405d62d43adf9f6aeac420943e84
DoyteDoyenr <eduardomedeirosgx@gmail.com>
2024-10-07 17:18:15 -0300

    6 commit

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/logoo.png'
 import Name from '../../assets/name.png'
 
 export default function AuthLayout({
